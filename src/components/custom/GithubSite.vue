7451b01eb3c4ccb7216cfb43c7b9c229b6c50e51
AnCup <117345856+ancuper@users.noreply.github.com>
2023-07-30 12:53:26 +0800

    Update GithubSite.vue

--- src/components/custom/GithubSite.vue
+++ src/components/custom/GithubSite.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="text-neutral-400">
     <span>尝试</span>
-    <a href="https://donate.ianime.one" target="_blank" class="text-blue-500">
+    <a href="https://ianime.one/donate.html" target="_blank" class="text-blue-500">
       捐助
     </a>
   </div>
